74ebab10f5983a70c6e43542b46b302602ee05cd
Vinayak P <pvinayak96@gmail.com>
2024-07-11 15:00:35 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AppService } from './app.service';
+import { CommonModule } from '@angular/common';  
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  constructor(private appService : AppService){
+
+  }
+
   title = 'gooApp';
+  interest !: string;
+  budgetType !: string;
+  tripType !: string;
+  place !: string;
+
+  output !: string;
+
+  loader : boolean = false; 
+
+  callGemini(){
+    this.loader = true;
+    this.appService.getAI("place: "+this.place + " , " + "trip type: "+this.tripType + " , "+ "budget type: " + this.budgetType + " , "+ "interest: "+ this.interest  ).subscribe(
+      res => {this.output = res.toString().replaceAll("**",'<b>'); this.loader=false}
+    )
+  }
 }
